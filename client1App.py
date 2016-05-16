e12e12c38f22149e7bb893e2a3dae40d6f6f37e6
sonicaj <waqarahmedjoyia@live.com>
2016-05-16 17:08:02 +0500

    Update client1App.py

--- client1App.py
+++ client1App.py
@@ -2,8 +2,6 @@ import socket
 from time import sleep
 
 if __name__ == '__main__':
-
-    print('hello')
     s = socket.socket()
 
     host = socket.gethostname()
@@ -19,7 +17,8 @@ if __name__ == '__main__':
     print('Connection with client established ' if data[2:len(data)-1] == 'Connection established' else ' ')
     if data[2:len(data) - 1] == 'Connection established':
         sleep(5)
-    while True:
+    checkConnection = True
+    while checkConnection:
         if data[2:len(data)-1] == 'Requested client has not connected yet, Please wait':
             print(data[2:len(data) - 1])
             data = s.recv(1024)
@@ -30,12 +29,20 @@ if __name__ == '__main__':
             print('Requested client rejected connection')
             break
         else:
-            data = s.recv(1024)
-            data = repr(data)
-            if data[2:5] == 'end':print('Connection terminated'); break
-            print('Client has a message for you  : \n', data[2:len(data) - 1])
-            data = str(input('Please reply what you want to say to Client ( to end connection type end ) : '))
-            data = bytes(data, 'utf8')
-            s.sendall(data)
+            checkSend,checkRecieve = True,True
             print('Waiting for Client to respond')
+            while checkRecieve:
+                data = s.recv(1024)
+                data = repr(data)
+                if data[2:5] == 'end':print('Connection terminated'); checkConnection,checkSend = False,False;break
+                print('Client has a message for you  : \n', data[2:len(data) - 1])
+                if data[len(data) - 2:len(data)-1] == '.':
+                    checkRecieve = False
+            while checkSend :
+                data = str(input('What do you want to say to Client ( to end connection type end ) : '))
+                if data[len(data) - 1:] == '.':checkSend = False
+                data = bytes(data, 'utf8')
+                s.sendall(data)
+                data = repr(data)
+                if data[2:5] == 'end': print('Connection terminated');checkConnection = False;break
     s.close()
