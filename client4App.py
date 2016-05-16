c336c6e3573ada8ce7d056f8595b25eb1abcf3ae
sonicaj <waqarahmedjoyia@live.com>
2016-05-16 17:09:43 +0500

    Update client4App.py

--- client4App.py
+++ client4App.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
     data = s.recv(1024)
     data = repr(data)
     print('Connection with client established ' if data[2:len(data) - 1] == 'Connection established' else ' ')
-    while True:
+    checkConnection = True
+    while checkConnection:
         if data[2:len(data)-1] == 'Requested client has not connected yet, Please wait':
             print('Requested client has not connected yet, Please wait')
             data = s.recv(1024)
@@ -25,12 +26,21 @@ if __name__ == '__main__':
             print('Requested client rejected connection')
             break
         else:
-            data = str(input('Please input the data you want to send to client ( to end connection type end ) : '))
-            data = bytes(data, 'utf8')
-            s.sendall(data)
-            print('Waiting for Client to respond')
-            data = s.recv(1024)
-            data = repr(data)
-            if data[2:5] == 'end': print('Connection terminated');break
-            print('Client has a message for you  : \n', data[2:len(data) - 1])
+            checkSend,checkRecieve = True,True
+            while checkSend:
+                data = str(input('Please input the data you want to send to client ( to end connection type end ) : '))
+                if data[len(data)-1:] == '.':
+                    checkSend = False
+                data = bytes(data, 'utf8')
+                s.sendall(data)
+                data = repr(data)
+                if data[2:5] == 'end': print('Connection terminated');checkConnection, checkRecieve = False, False;break
+            print('Waiting for cient to respond' if checkRecieve else '')
+            while checkRecieve:
+                data = s.recv(1024)
+                data = repr(data)
+                if data[len(data)-2:len(data)-1] == '.':
+                    checkRecieve = False
+                if data[2:5] == 'end': print('Connection terminated');checkConnection, checkRecieve = False, False;break
+                print('Client has a message for you  : \n', data[2:len(data) - 1])
     s.close()
